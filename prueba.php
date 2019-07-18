d83774da8d759c5b70601dfc87b0f796b5481640
Jose Arias Melo <36265049+joceduardoarias@users.noreply.github.com>
2019-07-18 12:28:00 -0300

    Add files via upload
    utilizamos sublimetex, wampp y xampp.

---
+++ prueba.php
@@ -0,0 +1,16 @@
+<html>
+
+  <body>
+	<?php
+	  $num = 7; // no es tipado no se de
+	 // echo "la variable tiene el valor <br>", $num //concatena con la coma para mostrar el valor 
+	  $cadena = "hola";
+	   $nombre = "jose"; 
+	  // echo $cadena	, " ", $nombre
+	   print"nombre: $nombre";
+	  
+	   }
+	?>
+  </body>
+
+</html>
