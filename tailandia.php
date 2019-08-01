4336e534c244ab14a78b2aef5bb1f19695ce7db5
Jose Arias Melo <36265049+joceduardoarias@users.noreply.github.com>
2019-08-01 12:23:10 -0300

    ejercicio contenido dinamico html con php

---
+++ tailandia.php
@@ -0,0 +1,53 @@
+<?php
+
+	$locacion = array("chinatown",
+					"floating-market", 
+					"hat-laem",
+					"wat-arun",
+					"khao-san-road",
+					"ko-khai-beach",
+					"kwai-river",
+					"limestone",
+					"longtail-boat",
+					"maya-bay",
+					"pra-nang",
+					"rai-leh-beach",
+					"rawai",
+					"sirocco",
+					"tuk-tuk",
+					"wat-saket"
+					 );
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Tailandia</title>
+	<style type="text/css">
+		body{font-family: "Myriad Pro"; color: #52BAE0}
+		.contenedor{
+			width:100px; 
+			padding:5px; 
+			margin:2px; 
+			float:left; 
+			border: 1px solid #ccc;
+		}
+		h1{padding-left:30px;font-size: 35px}
+		h2{font-size: 11px; color: #555;}
+	</style>
+</head>
+<body>
+
+	<h1>Tailandia</h1>	
+	
+	<div class="contenedor">
+		imagen aquí
+		<br>
+		<h2>texto aquí</h2>	
+	</div>
+
+</body>
+</html>
+
